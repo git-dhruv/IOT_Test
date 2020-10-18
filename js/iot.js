70ff0d4a420f15b8645f85fcac63e9c6e6748797
Dhruv Parikh <36257390+git-dhruv@users.noreply.github.com>
2020-10-18 13:24:07 +0530

    Background image

--- js/iot.js
+++ js/iot.js
@@ -4,6 +4,8 @@ function change_to_iframe(framedata)
 	var temp = "https://thingspeak.com/channels/1191743/charts/1?dynamic=true&results=60&title=Temperature&type=line"
 	var moist = "https://thingspeak.com/channels/1186763/charts/1?bgcolor=%23ffffff&dynamic=true&results=60&type=line&update=15";
 	var gas = "https://thingspeak.com/channels/1186763/charts/1?bgcolor=%23ffffff&dynamic=true&results=60&type=line&update=15";
+	var tilt = "https://thingspeak.com/channels/1186763/charts/1?bgcolor=%23ffffff&dynamic=true&results=60&type=line&update=15";
+	var pir = "https://thingspeak.com/channels/1186763/charts/1?bgcolor=%23ffffff&dynamic=true&results=60&type=line&update=15";	
 	if(framedata=="temp")
 	{
 		framesrc = temp;
@@ -16,6 +18,15 @@ function change_to_iframe(framedata)
 	{
 		framesrc = gas
 	}
+	else if(framedata=="tilt")
+	{
+		framesrc=tilt
+	}
+	else if(framedata=="pir")
+	{
+		framesrc=pir
+	}
+	
 	document.getElementById(framedata).src=framesrc
 	document.getElementById(framedata).style.width = "450px";
 	document.getElementById(framedata).style.height ="260px";
@@ -23,7 +34,7 @@ function change_to_iframe(framedata)
 }
 function back_to_img(framedata)
 {
-	document.getElementById(framedata).style.width = "70px";
-	document.getElementById(framedata).style.height ="111px";
+	document.getElementById(framedata).style.width = "72px";
+	document.getElementById(framedata).style.height ="120px";
 	document.getElementById(framedata).src="media/images/"+framedata+".png";
 }
